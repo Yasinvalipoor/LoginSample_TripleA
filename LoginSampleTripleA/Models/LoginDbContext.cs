c001198cd4ae322ef3608a2f72134d5aa32a7827
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-05-14 11:01:37 +0330

    new

--- LoginSampleTripleA/Models/LoginDbContext.cs
+++ LoginSampleTripleA/Models/LoginDbContext.cs
@@ -10,6 +10,4 @@ namespace LoginSampleTripleA.Models
         {
         }
     }
-
-
 }
---
+++ LoginSampleTripleA/Pages/Users/UsersList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginSampleTripleA.Pages.Users
+{
+    public class UsersListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
