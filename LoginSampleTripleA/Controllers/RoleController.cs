3c2c355564e55c557b8eadbed34816bce3eec20a
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-05-14 22:10:09 +0330

    new change

--- LoginSampleTripleA/Controllers/RoleController.cs
+++ LoginSampleTripleA/Controllers/RoleController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using LoginSampleTripleA.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LoginSampleTripleA.Controllers
@@ -15,6 +16,70 @@ namespace LoginSampleTripleA.Controllers
         {
             var roles = _roleManager.Roles.ToList();
             return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityRole = new IdentityRole(model.Name);
+                var result = await _roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("List");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string Name)
+        {
+            var role = await _roleManager.FindByNameAsync(Name);
+            if (role != null)
+            {
+                var result = await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("List");
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            return View(role);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(IdentityRole model)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityRole = await _roleManager.FindByIdAsync(model.Id);
+                if (identityRole != null)
+                {
+                    identityRole.Name = model.Name;
+                    var result = await _roleManager.UpdateAsync(identityRole);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("List");
+                    }
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
