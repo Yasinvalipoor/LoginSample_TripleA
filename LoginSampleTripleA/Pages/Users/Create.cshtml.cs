89ca71f2802705712eda3cd67b5ff473ab9676b3
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-05-14 12:04:42 +0330

    new

--- LoginSampleTripleA/Pages/Users/Create.cshtml.cs
+++ LoginSampleTripleA/Pages/Users/Create.cshtml.cs
@@ -1,12 +1,53 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace LoginSampleTripleA.Pages.Users
 {
     public class CreateModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public CreateModel(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        [BindProperty]
+        public string UserName { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        [BindProperty]
+        [EmailAddress]
+        public string Email { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new()
+                {
+                    UserName = UserName,
+                    Email = Email
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToPage("List");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return Page();
         }
     }
 }
--- LoginSampleTripleA/Program.cs
+++ LoginSampleTripleA/Program.cs
@@ -7,29 +7,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddMvc();
 builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<LoginDbContext>(c=>c.UseSqlServer(builder.Configuration.GetConnectionString("LoginDbCnn")));
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<LoginDbContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseDeveloperExceptionPage();
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
+app.MapRazorPages();
 
-app.UseAuthorization();
+app.MapDefaultControllerRoute();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=HomeIndex}/{id?}");
+
 
 app.Run();
