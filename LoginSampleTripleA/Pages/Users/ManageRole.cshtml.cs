14a90dd7f6f4d08a2c87e6d90437692408599bfe
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-05-15 14:53:01 +0330

    New Feature

--- LoginSampleTripleA/Pages/Users/ManageRole.cshtml.cs
+++ LoginSampleTripleA/Pages/Users/ManageRole.cshtml.cs
@@ -13,6 +13,13 @@ namespace LoginSampleTripleA.Pages.Users
         public List<IdentityRole> AllRoles { get; set; }
         public List<string> UserRoles { get; set; }
 
+        [BindProperty]
+        public List<string> Roles { get; set; }
+
+        [BindProperty]
+        public string Id { get; set; }
+
+
         public ManageRoleModel(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             this._userManager = userManager;
@@ -21,9 +28,34 @@ namespace LoginSampleTripleA.Pages.Users
 
         public async Task OnGet(string id)
         {
-            var user = await _userManager.FindByIdAsync(id);
-            UserRoles = (await _userManager.GetRolesAsync(user)).ToList();
+            CurrentUser = await _userManager.FindByIdAsync(id);
+            UserRoles = (await _userManager.GetRolesAsync(CurrentUser)).ToList();
             AllRoles = _roleManager.Roles.ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            CurrentUser = await _userManager.FindByIdAsync(Id);
+            AllRoles = _roleManager.Roles.ToList();
+
+            foreach (var item in AllRoles)
+            {
+                if (Roles.Contains(item.Name))
+                {
+                    if (!(await _userManager.IsInRoleAsync(CurrentUser, item.Name)))
+                    {
+                        await _userManager.AddToRoleAsync(CurrentUser, item.Name);
+                    }
+                }
+                else
+                {
+                    if (await _userManager.IsInRoleAsync(CurrentUser, item.Name))
+                    {
+                        await _userManager.RemoveFromRoleAsync(CurrentUser, item.Name);
+                    }
+                }
+            }
+            return RedirectToPage("List");
         }
     }
 }
