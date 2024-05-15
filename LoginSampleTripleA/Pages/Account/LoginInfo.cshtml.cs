f7bbd99b0b80ee6d883c4dc66ad9729406b0e80d
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-05-15 18:29:52 +0330

    fix

---
+++ LoginSampleTripleA/Pages/Account/LoginInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginSampleTripleA.Pages.Account
+{
+    public class LoginInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- LoginSampleTripleA/Program.cs
+++ LoginSampleTripleA/Program.cs
@@ -39,6 +39,8 @@ app.UseStaticFiles();
 
 app.MapRazorPages();
 
+app.UseAuthentication();
+
 app.MapDefaultControllerRoute();
 
 
