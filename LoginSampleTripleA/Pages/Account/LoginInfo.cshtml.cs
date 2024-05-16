7f1bf6f5c808cdebd77130276ce2ab86e8118ff1
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-05-16 11:42:48 +0330

    Authentication - Authorization

--- LoginSampleTripleA/Pages/Account/LoginInfo.cshtml.cs
+++ LoginSampleTripleA/Pages/Account/LoginInfo.cshtml.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LoginSampleTripleA.Pages.Account
 {
+    [Authorize]
     public class LoginInfoModel : PageModel
     {
+
         public void OnGet()
         {
         }
